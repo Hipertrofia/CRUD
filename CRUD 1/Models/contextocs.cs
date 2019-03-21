1cfcd2d4a4b10cb2ea885cae7cc11f51a2d36a0b
unknown <jansen@dcc.ufrj.br>
2019-03-21 19:23:25 -0300

    Meu Primeiro CRUD

---
+++ CRUD 1/Models/contextocs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace CRUD_1.Models
+{
+    public class contextocs: DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        
+    }
+}
